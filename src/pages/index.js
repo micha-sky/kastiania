564adffe5c0fcb0a842c8584663cbb852e12caed
Oleksandr Mykhalskyi <oleksandr.mykhalskyi@unrulygroup.com>
2023-03-13 17:11:26 +0100

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import  styles from  "../css/main.css"
 export default function Home() {
-  return <div>Kastania Waldmüller</div>
+  return <div style={styles}>Kastania Waldmüller</div>
 }
